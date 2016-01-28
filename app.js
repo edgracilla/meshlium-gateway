ab5cadb61f23c34e9398e5a98ed7cb222e4772fe
Benj Sicam <bsicam@reekoh.com>
2016-01-28 11:28:19 +0800

    Stable Version
    - Data Parsing
    - Config enhancements

--- app.js
+++ app.js
@@ -3,28 +3,7 @@
 var isEmpty           = require('lodash.isempty'),
 	platform          = require('./platform'),
 	authorizedDevices = {},
-	server, port, qos;
-
-/*
- * Listen for the message event.
- */
-platform.on('message', function (message) {
-	server.publish({
-		topic: message.device,
-		payload: message.message,
-		messageId: message.messageId,
-		qos: qos,
-		retain: false
-	}, function () {
-		platform.sendMessageResponse(message.messageId, 'Message Published');
-		platform.log(JSON.stringify({
-			title: 'Message Published',
-			device: message.device,
-			messageId: message.messageId,
-			message: message.message
-		}));
-	});
-});
+	server, port;
 
 /*
  * When a new device is added, add it to the list of authorized devices.
@@ -54,8 +33,7 @@ platform.on('removedevice', function (device) {
  * Event to listen to in order to gracefully release all resources bound to this service.
  */
 platform.on('close', function () {
-	var domain = require('domain');
-	var d = domain.create();
+	let d = require('domain').create();
 
 	d.once('error', function (error) {
 		console.error('Error closing Meshlium Gateway on port ' + port, error);
@@ -67,7 +45,6 @@ platform.on('close', function () {
 	d.run(function () {
 		server.close();
 		console.log('Meshlium Gateway closed on port ' + port);
-		platform.notifyClose();
 		d.exit();
 	});
 });
@@ -76,112 +53,58 @@ platform.on('close', function () {
  * Listen for the ready event.
  */
 platform.once('ready', function (options, registeredDevices) {
-	var map    = require('lodash.map'),
-		trim   = require('lodash.trim'),
-		uniq   = require('lodash.uniq'),
-		keyBy  = require('lodash.keyby'),
+	var keyBy  = require('lodash.keyby'),
 		mosca  = require('mosca'),
 		domain = require('domain'),
 		config = require('./config.json');
 
-	if (options.qos === 0 || isEmpty(options.qos))
-		qos = 0;
-	else
-		qos = parseInt(options.qos);
-
 	if (!isEmpty(registeredDevices))
 		authorizedDevices = keyBy(registeredDevices, '_id');
 
-	var dataTopic = options.data_topic || config.data_topic.default;
-	var messageTopic = options.message_topic || config.message_topic.default;
-	var groupMessageTopic = options.groupmessage_topic || config.groupmessage_topic.default;
-
-	var tmpTopics = [dataTopic, messageTopic, groupMessageTopic];
-	var authorizedTopics = options.authorized_topics;
-
-	if (!isEmpty(authorizedTopics))
-		authorizedTopics = tmpTopics.concat(authorizedTopics.split(','));
-	else
-		authorizedTopics = tmpTopics;
-
-	authorizedTopics = map(authorizedTopics, trim);
-	authorizedTopics = keyBy(uniq(authorizedTopics));
+	var topic = options.topic || config.topic.default;
 
 	port = options.port;
 	server = new mosca.Server({
 		port: port
 	});
 
-	server.on('clientConnected', function (client) {
-		platform.notifyConnection(client.id);
-	});
-
-	server.on('clientDisconnected', function (client) {
-		platform.notifyDisconnection(client.id);
-	});
-
 	server.on('published', function (message, client) {
-		var msg = message.payload.toString();
+		if (message.topic === topic) {
+			let d = domain.create();
 
-		if (message.topic === dataTopic) {
-			platform.processData(client.id, JSON.stringify({
-				data: msg
-			}));
 
-			platform.log(JSON.stringify({
-				title: 'Data Received.',
-				device: client.id,
-				data: msg
-			}));
-		}
-		/*else if (message.topic === messageTopic) {
-			var d1 = domain.create();
+			d.once('error', (error) => {
+				platform.handleException(error);
 
-			d1.once('error', function () {
-				platform.log(new Error('Invalid message received. Raw Message: ' + msg));
-				d1.exit();
+				d.exit();
 			});
 
-			d1.run(function () {
-				msg = JSON.parse(msg);
+			d.run(() => {
+				let msg = message.payload.toString(),
+					obj = JSON.parse(msg);
 
-				platform.sendMessageToDevice(msg.target, msg.message);
+				if (isEmpty(obj.id_wasp)) return d.exit();
+
+				if (isEmpty(authorizedDevices[obj.id_wasp])) {
+					platform.log(JSON.stringify({
+						title: 'Unauthorized Device',
+						device: obj.id_wasp
+					}));
+
+					return d.exit();
+				}
+
+				platform.processData(obj.id_wasp, msg);
+
 				platform.log(JSON.stringify({
-					title: 'Message Sent.',
-					source: client.id,
-					target: msg.target,
-					message: msg
+					title: 'Meshlium Data Received.',
+					device: client.id,
+					data: obj
 				}));
 
-				d1.exit();
+				d.exit();
 			});
 		}
-		else if (message.topic === groupMessageTopic) {
-			var d2 = domain.create();
-
-			d2.once('error', function () {
-				platform.log(new Error('Invalid group message received. Raw Message: ' + msg));
-				d2.exit();
-			});
-
-			d2.run(function () {
-				msg = JSON.parse(msg);
-
-				platform.sendMessageToGroup(msg.target, msg.message);
-				platform.log(JSON.stringify({
-					title: 'Group Message Sent.',
-					source: client.id,
-					target: msg.target,
-					message: msg
-				}));
-
-				d2.exit();
-			});
-		}*/
-	});
-
-	server.on('delivered', function (message) {
-		platform.sendMessageResponse(message.messageId, 'Message Acknowledged');
 	});
 
 	server.on('closed', function () {
@@ -197,16 +120,16 @@ platform.once('ready', function (options, registeredDevices) {
 	});
 
 	server.on('ready', function () {
-		server.authorizePublish = function (client, topic, payload, callback) {
-			return callback(null, !isEmpty(authorizedDevices[client.id]) || topic === client.id || !isEmpty(authorizedTopics[topic]));
-		};
+		server.authenticate = function (client, username, password, callback) {
+			username = (!isEmpty(username)) ? username.toString() : '';
+			password = (!isEmpty(password)) ? password.toString() : '';
 
-		server.authorizeSubscribe = function (client, topic, callback) {
-			return callback(null, !isEmpty(authorizedDevices[client.id]) || topic === client.id || !isEmpty(authorizedTopics[topic]));
-		};
-
-		server.authorizeForward = function (client, packet, callback) {
-			return callback(null, !isEmpty(authorizedDevices[client.id]));
+			if (options.user !== username || options.password !== password) {
+				platform.log(`Authentication Failed on Client: ${(!isEmpty(client)) ? client.id : 'No Client ID'}.`);
+				callback(null, false);
+			}
+			else
+				return callback(null, true);
 		};
 
 		platform.log('Meshlium Gateway initialized on port ' + port);
